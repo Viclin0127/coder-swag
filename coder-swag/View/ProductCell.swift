0f73b6964c7b9eef5ae9563240122720d029dcec
YuanHungLin <viclin@linyuanhongde-MacBook-Pro.local>
2019-01-26 22:28:46 +0800

    add collectionViewCell

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by YuanHungLin on 2019/1/26.
+//  Copyright © 2019 linyuanhung. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
